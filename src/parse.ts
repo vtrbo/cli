c1b3a6a177970ecf62355d154433c26215134243
Victor Bo <10667379@qq.com>
2022-07-22 12:29:58 +0800

    chore: update write style

--- src/parse.ts
+++ src/parse.ts
@@ -7,23 +7,52 @@ import type { Runner } from './types'
 export const parse = <Runner>((args, cwd) => {
   console.log('args, cwd', args, cwd)
 
-  if (args.length === 1 && ['-h', '--help'].includes(args[0])) {
-    console.log(`
-    -v --version 版本
-    -d --download 下载
-    `)
-    process.exit(0)
-  }
-
-  if (args.length === 1 && ['-v', '--version'].includes(args[0])) {
-    console.log(`@vtrbo/cli v${version}`)
-    process.exit(0)
-  }
-
-  if (args.length === 1 && ['-i', '--init', 'init', 'create', 'start'].includes(args[0])) {
-    console.log('问答式命令行')
-    return 'interaction'
-  }
+  if (args.length === 1)
+    return singleArgs(args)
 
   return 'ni'
 })
+
+/**
+ * @description 单个参数的处理函数
+ * @param args 参数
+ */
+function singleArgs(args: string[]): string | void {
+  const strategy: {
+    [key: string]: () => string | void
+  } = {
+    '-h': commandHelp,
+    '--help': commandHelp,
+    '-v': commandVersion,
+    '--version': commandVersion,
+    '-i': commandInteraction,
+    '--init': commandInteraction,
+    'init': commandInteraction,
+    'create': commandInteraction,
+    'start': commandInteraction,
+  }
+  return strategy[args[0]]()
+}
+
+/**
+ * @description 帮助信息
+ */
+function commandHelp() {
+  console.log('帮助信息')
+  process.exit(0)
+}
+
+/**
+ * @description 版本信息
+ */
+function commandVersion() {
+  console.log(`@vtrbo/cli v${version}`)
+  process.exit(0)
+}
+
+/**
+ * @description 问答式命令行
+ */
+function commandInteraction(): string {
+  return 'interaction'
+}
